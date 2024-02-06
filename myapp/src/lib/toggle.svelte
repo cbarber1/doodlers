429eb6d1afc0352cc2bcc3755a1f99af4ed58ace
Jessica Li <jessicali1703@gmail.com>
2024-02-05 19:17:51 -0500

    toggle

---
+++ myapp/src/lib/toggle.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import SegmentedButton, { Segment } from "@smui/segmented-button";
+  import { Label } from "@smui/common";
+
+  let choices = ["Availability", "Preferences"];
+  let selected = "Availability";
+</script>
+
+<div class="segmented-button-wrapper">
+  <SegmentedButton segments={choices} let:segment singleSelect bind:selected>
+    <Segment {segment}>
+      <Label>{segment}</Label>
+    </Segment>
+  </SegmentedButton>
+</div>
+
+<style>
+  .segmented-button-wrapper {
+    margin-right: 120px;
+  }
+</style>
--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,17 +1,22 @@
 <script>
-	import Table_input from './Table_input.svelte';
+  import Table_input from "./Table_input.svelte";
+  import Toggle from "../lib/toggle.svelte";
 </script>
 
-<h1>Doodlers Epic Page</h1>
+<h1 class="text-center p-4">Doodlers Epic Page</h1>
 
 <div class="container">
-<div class="row">
+  <div class="row">
+    <div class="col d-flex justify-content-end m-4">
+      <Toggle />
+    </div>
+  </div>
+  <div class="row">
     <div class="col">
-    <Table_input />
+      <Table_input />
     </div>
     <div class="col">
-    <Table_input />
+      <Table_input />
     </div>
-</div>
-
+  </div>
 </div>
