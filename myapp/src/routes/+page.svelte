502e8bfa70475a5beaac02c852ccdf47b769fe76
Sylvia <sylcrza@gmail.com>
2024-02-05 21:19:00 -0500

    add toggle from Svelte Component library

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,15 +1,11 @@
 <script>
 	import Table_input from './Table_input.svelte';
-    import Toggle from "../lib/toggle.svelte";
 </script>
 
 <h1 class="text-center p-4">Doodlers Epic Page</h1>
 
 <div class="container">
   <div class="row">
-    <div class="col d-flex justify-content-end m-4">
-      <Toggle />
-    </div>
   </div>
   <div class="row">
     <div class="col">
